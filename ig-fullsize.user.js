3cb01239112d4a67659ed7a87247189b4f69bfc1
realies <5107843+realies@users.noreply.github.com>
2019-04-12 12:33:40 +0100

    fix carousel element detection

--- ig-fullsize.user.js
+++ ig-fullsize.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         IG Fullsize
-// @version      0.2
+// @version      0.3
 // @description  View full resolution images on Instagram
 // @match        https://www.instagram.com/*
 // @require      https://gist.githubusercontent.com/realies/2fece0cd3e197cf6b31ca1316431a2a4/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
 // @author       realies
 // ==/UserScript==
 (() => {
-    waitForKeyElements("div.kPFhm", viewBox => {
+    waitForKeyElements("div.eLAPa", viewBox => {
         let clickBox = viewBox.querySelector("div._9AhH0");
         clickBox.style.cursor = "zoom-in";
         clickBox.onclick = () => window.open(viewBox.querySelector("img.FFVAD").src, "_blank");
