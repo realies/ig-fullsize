5cfbff405124924ce12f02dd543b992ec923fea3
realies <5107843+realies@users.noreply.github.com>
2022-06-04 22:55:33 +0100

    revert to using src for image location

--- ig-fullsize.user.js
+++ ig-fullsize.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         IG Fullsize
-// @version      0.4
+// @version      0.5
 // @description  View full resolution images on Instagram
 // @match        https://www.instagram.com/*
 // @require      https://gist.githubusercontent.com/realies/2fece0cd3e197cf6b31ca1316431a2a4/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
@@ -9,6 +9,6 @@
 (() => {
     waitForKeyElements("div._aagu", clickBox => {
         clickBox.style.cursor = "zoom-in";
-        clickBox.onclick = () => window.open(clickBox.querySelector("img._aagt").srcset.split(' ').shift());
+        clickBox.onclick = () => window.open(clickBox.querySelector("img._aagt").src);
     }, false);
 })();
