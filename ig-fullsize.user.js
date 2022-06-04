c090a9ed92a2e14197c86bdb008f71a6c7613ecd
realies <5107843+realies@users.noreply.github.com>
2022-06-04 21:05:29 +0100

    fix element detection, use srcset for largest image

--- ig-fullsize.user.js
+++ ig-fullsize.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         IG Fullsize
-// @version      0.3
+// @version      0.4
 // @description  View full resolution images on Instagram
 // @match        https://www.instagram.com/*
 // @require      https://gist.githubusercontent.com/realies/2fece0cd3e197cf6b31ca1316431a2a4/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
 // @author       realies
 // ==/UserScript==
 (() => {
-    waitForKeyElements("div.eLAPa", viewBox => {
-        let clickBox = viewBox.querySelector("div._9AhH0");
+    waitForKeyElements("div._aagu", clickBox => {
         clickBox.style.cursor = "zoom-in";
-        clickBox.onclick = () => window.open(viewBox.querySelector("img.FFVAD").src, "_blank");
+        clickBox.onclick = () => window.open(clickBox.querySelector("img._aagt").srcset.split(' ').shift());
     }, false);
 })();
