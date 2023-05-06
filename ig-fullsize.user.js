d6debf11605f20fe46ae853f9c64bc84d0ef2c9e
realies <5107843+realies@users.noreply.github.com>
2023-05-06 01:32:55 +0100

    fix image selector

--- ig-fullsize.user.js
+++ ig-fullsize.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         IG Fullsize
-// @version      0.5
+// @version      0.6
 // @description  View full resolution images on Instagram
 // @match        https://www.instagram.com/*
 // @require      https://gist.githubusercontent.com/realies/2fece0cd3e197cf6b31ca1316431a2a4/raw/debc0e6d4d537ac228d1d71f44b1162979a5278c/waitForKeyElements.js
@@ -9,6 +9,6 @@
 (() => {
     waitForKeyElements("div._aagu", clickBox => {
         clickBox.style.cursor = "zoom-in";
-        clickBox.onclick = () => window.open(clickBox.querySelector("img._aagt").src);
+        clickBox.onclick = () => window.open(clickBox.querySelector("img").src);
     }, false);
 })();
